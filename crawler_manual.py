ffd673a1fe39cd1c06cb0b486efd96824218b957
Chandler Zuo <chandlerzuo@fb.com>
2025-10-20 01:09:03 +0000

    enable async crawling

--- crawler_manual.py
+++ crawler_manual.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
-from typing import Set, Optional
+from typing import Set
 from urllib.parse import urljoin, urldefrag, urlparse
 from urllib.robotparser import RobotFileParser
 import requests
-from collections import deque
 import os
 import time
+import queue
+import threading
 
 
 def get_outbound_links(text: str, base_url: str) -> Set[str]:
@@ -27,6 +28,7 @@ def get_outbound_links(text: str, base_url: str) -> Set[str]:
     return ret
   return ret
 
+
 def construct_filepath(output_dir: str, domain: str, path: str) -> str:
   filepath = os.path.join(output_dir, domain, path.lstrip("/"))
   if path == "" or path.endswith("/"):
@@ -38,51 +40,110 @@ def construct_filepath(output_dir: str, domain: str, path: str) -> str:
     pass
   return filepath
 
+
 class Crawler:
 
   def __init__(self, seed_url: str, delay: float, output_dir: str,
-               max_pages: int):
+               max_pages: int, num_workers: int):
     self.seed_url = seed_url
     self.session = requests.Session()
     self.visited_url: Set[str] = set()
-    self.queue = deque([seed_url])
+    self.queue = queue.Queue()
+    self.queue.put(seed_url)
     self.max_pages = max_pages
     self.output_dir = output_dir
     if not os.path.exists(output_dir):
       os.makedirs(output_dir)
     self.agent = "WebCrawler/1.0 Educational Purpose"
     self.delay = delay
+    self.num_workers = num_workers
+    self.last_request_time = time.time()
+    self.finish_crawl = threading.Event()
+
+    self.queue_lock = threading.Lock()
+    self.visited_lock = threading.Lock()
+    self.rate_limit_lock = threading.Lock()
 
   def fetch_page(self, url: str):
     outbound_links: Set[str] = set()
     try:
+      with self.rate_limit_lock:
+        elapsed = time.time() - self.last_request_time
+        if elapsed < self.delay:
+          time.sleep(self.delay - elapsed)
+          self.last_request_time = time.time()
       response = self.session.get(url, timeout=10, allow_redirects=True)
       text = response.text
       outbound_links = get_outbound_links(text, url)
-      outbound_links = outbound_links - self.visited_url
-      self.queue.extend(outbound_links)
+      with self.visited_lock:
+        outbound_links = outbound_links - self.visited_url
+        with self.queue_lock:
+          for link in outbound_links:
+            if not self.finish_crawl.is_set():
+              print(f"thread {threading.current_thread().name} put {link}")
+              self.queue.put(link)
       # write to file
       parsed = urlparse(url)
-      filepath = construct_filepath(self.output_dir, parsed.netloc, parsed.path)
+      filepath = construct_filepath(self.output_dir, parsed.netloc,
+                                    parsed.path)
       with open(filepath, "w") as f:
-        print(f"Writing {url} to file")
         f.write(text)
     except Exception as e:
       print(f"Error fetching page {str(url)}: {str(e)}")
 
+  def worker(self, max_pages: int):
+    print(f"Starting worker {threading.current_thread().name}")
+    num_pages = 0
+    while True:
+      try:
+        url = self.queue.get(timeout=1)
+      except queue.Empty:
+        if self.finish_crawl.is_set():
+          break
+        continue
+      print(f"Worker {threading.current_thread().name} is crawling {url}")
+      with self.visited_lock:
+        if len(self.visited_url) >= max_pages:
+          print(f"Task done for {url} as max pages reached.")
+          self.queue.task_done()
+          if not self.finish_crawl.is_set():
+            self.finish_crawl.set()
+          ## NOTE: have to continue here, to continue to deque until the queue is empty
+          continue
+        if url in self.visited_url:
+          print(f"Task done for {url} as already visited.")
+          self.queue.task_done()
+          continue
+        self.visited_url.add(url)
+      try:
+        print(f"Worker {threading.current_thread().name} is parsing {url}")
+        # extract domain
+        parsed = urlparse(url)
+        # check if robots.txt exists
+        rp = RobotFileParser(
+            urljoin(f"{parsed.scheme}://{parsed.netloc}", "robots.txt"))
+        rp.read()
+        if rp.can_fetch(self.agent, url):
+          self.fetch_page(url)
+        num_pages += 1
+        print(f"task done for {url}")
+      finally:
+        self.queue.task_done()
+    print(
+        f"Worker {threading.current_thread().name} finished, downloaded {num_pages} pages."
+    )
+
   def crawl(self):
-    while self.queue and len(self.visited_url) < self.max_pages:
-      url = self.queue.popleft()
-      self.visited_url.add(url)
-      # extract domain
-      parsed = urlparse(url)
-      # check if robots.txt exists
-      rp = RobotFileParser(
-          urljoin(f"{parsed.scheme}://{parsed.netloc}", "robots.txt"))
-      rp.read()
-      if rp.can_fetch(self.agent, url):
-        get_outbound_links = self.fetch_page(url)
-        time.sleep(self.delay)
+    threads = []
+    for i in range(self.num_workers):
+      t = threading.Thread(target=self.worker, args=(self.max_pages, ))
+      t.start()
+      threads.append(t)
+    self.queue.join()
+    self.finish_crawl.set()
+    print("All tasks are done")
+    for t in threads:
+      t.join(timeout=10)
 
 
 def test_get_outbound_links():
@@ -121,8 +182,11 @@ def test_get_outbound_links():
 
 
 def download():
-  crawler = Crawler("https://chanderzuo.github.io", 0.1,
-                    "output/chandlerzuo.github.io", 10)
+  crawler = Crawler("https://chanderzuo.github.io",
+                    0.1,
+                    "output/chandlerzuo.github.io",
+                    2,
+                    num_workers=2)
   crawler.crawl()
   print("Results saved to chandlerzuo.github.io")
 
